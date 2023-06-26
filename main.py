3c1fe60af9fc3bf3d1aec849ecb817e6520a0926
Asus <asus@pop-os.localdomain>
2023-06-26 18:59:35 +0700

    add result png

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from test_main import *
 
 ITERATION = 500
 population = generate_population(100)
+best_fitness = []
 for i in range(ITERATION):
-    evolve(population)
-    print_stats(population, i)
+    new_population = evolve(population)
+    best = print_stats(new_population, i)
+    f = fitness_function(best)
+    best_fitness.append(f)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(ITERATION)
+y = np.array(best_fitness)
+
+plt.plot(x, y)
+plt.savefig('result')
+plt.show()
